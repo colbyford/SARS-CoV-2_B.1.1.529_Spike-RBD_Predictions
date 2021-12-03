7f01e8d9a76165d1fb5d4893c52a4b0ca03ca361
Colby Ford <colbytylerford@gmail.com>
2021-12-03 14:04:32 -0500

    Create wilcoxon_test.R

---
+++ docking/wilcoxon_test.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(readxl)
+
+data <- read_xlsx("HADDOCK_Results.xlsx")
+
+# data_ref <- data %>% filter(`Analysis Type` == "Reference")
+# data_pred <- data %>% filter(`Analysis Type` == "Prediction")
+
+## Wilcox Test
+
+pairwise.wilcox.test(data$`Desolvation energy`, data$`Analysis Type`, p.adjust.method="bonferroni")
+pairwise.wilcox.test(data$`Electrostatic energy`, data$`Analysis Type`, p.adjust.method="bonferroni")
+pairwise.wilcox.test(data$`Restraints violation energy`, data$`Analysis Type`, p.adjust.method="bonferroni")
+pairwise.wilcox.test(data$`Van der Waals energy`, data$`Analysis Type`, p.adjust.method="bonferroni")
+pairwise.wilcox.test(data$`HADDOCK score`, data$`Analysis Type`, p.adjust.method="bonferroni")
+pairwise.wilcox.test(data$`Buried Surface Area`, data$`Analysis Type`, p.adjust.method="bonferroni")
+
